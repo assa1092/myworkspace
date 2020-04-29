f60ca080f4f813fe00b92112475e2954607d1f3c
assa1092 <vincent1092@naver.com>
2020-04-29 17:42:28 +0900

    project game reply

--- eclipse-workspace/SimpleExam/src/com/naver/Hero.java
+++ eclipse-workspace/SimpleExam/src/com/naver/Hero.java
@@ -1,25 +1,33 @@
 package com.naver;
 
 public class Hero {
-	private Weapon[] weapons;
-	private Weapon hasweapon;
+	private Weapon[] weapons;	// 가지고 있는 무기들
+	private Weapon hasweapon;	// 손에 있는 무기
 	
+	// 디폴트 생성자
 	public Hero() {
-		weapons =new Weapon[] {new Bow("활",1), new Sword("검",5), new Hammer("망치",10)};
-		hasweapon = weapons[0];
+		// 세가지의 무기를 가지고 있음
+		weapons = new Weapon[] {new Bow("활",1), new Sword("검",5), new Hammer("망치",10)};
+		hasweapon = weapons[0];		// 기본으로 활을 가지고 있음.
 	}
-
+	
+	// 사용자 정의 생성자
 	public Hero(Weapon[] weapons, int idx) {
 		super();
 		this.weapons = weapons;
 		this.hasweapon = weapons[idx];
 	}
 	
+	// 몬스터 공격하기
 	public void attack(Monster m) {
-		hasweapon.attack(m);
+		hasweapon.attack(m);	// 가지고 있는 무기로 공격하기
 	}
 	
-	public void changeWeapon(int idx) {
+	public void changeWeapon(int idx) throws NoHaveWeaponException {
+		
+		if(idx >= weapons.length) {
+			throw new NoHaveWeaponException();
+		}
 		hasweapon = weapons[idx];
 		System.out.println(hasweapon);
 	}
---
+++ eclipse-workspace/SimpleExam/src/com/naver/NoHaveWeaponException.java
@@ -0,0 +1,12 @@
+package com.naver;
+
+public class NoHaveWeaponException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+
+}
