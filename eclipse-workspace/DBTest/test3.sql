ad321cf6e2c3689097e8b15641635afbb4bfbc0c
assa1092 <vincent1092@naver.com>
2020-05-08 17:40:19 +0900

    primary key,foreign key, constraint, references

---
+++ eclipse-workspace/DBTest/test3.sql
@@ -0,0 +1,101 @@
+create table member(
+id varchar2(6),
+name varchar2(21) not null,
+age number(3) check(age<200)		
+)
+// check(age<200) age가 200 이하만 가능...
+
+insert into member(id, name, age) values ('m001', 'a',150)
+insert into member(id, name, age) values ('m002', 'b',151)
+insert into member(id, name, age) values ('m003', 'c',152)
+insert into member(id, name, age) values ('m004', 'd',153)
+insert into member(id, name, age) values ('m005', 'e',154)
+insert into member(id, name, age) values ('m006', 'f',155)
+insert into member(id, name, age) values ('m007', 'g',156)
+insert into member(id, name, age) values ('m008', 'h',157)
+insert into member(id, name, age) values ('m009', 'i',158)
+insert into member(id, name, age) values ('m010', 'j',159)
+insert into member(id, name, age) values ('m011', 'k',160)
+insert into member(id, name, age) values ('m012', 'l',161)
+insert into member(id, name, age) values ('m013', 'm',162)
+insert into member(id, name, age) values ('m014', 'n',163)
+insert into member(id, name, age) values ('m015', 'o',164)
+insert into member(id, name, age) values ('m016', 'p',165)
+insert into member(id, name, age) values ('m017', 'q',166)
+insert into member(id, name, age) values ('m018', 'r',167)
+insert into member(id, name, age) values ('m019', 's',168)
+insert into member(id, name, age) values ('m020', 't',169)
+insert into member(id, name, age) values ('m021', 'u',170)
+insert into member(id, name, age) values ('m022', 'v',171)
+insert into member(id, name, age) values ('m023', 'w',172)
+insert into member(id, name, age) values ('m024', 'x',173)
+insert into member(id, name, age) values ('m025', 'y',174)
+insert into member(id, name, age) values ('m026', 'z',175)
+
+select * from member where id='m020'
+select id from member
+
+// member테이블에 있는 모든 레코드를 조회하는데 id와 name컬럼 데이터만 가져오시오
+select id,name from member
+
+// member테이블에 있는 모든 레코드를 조회하는데 모든 컬럼 정보를 다 가져옵니다. 
+// 그런데 age에 +1을 해서 가져오세요
+select id, name, age+1 from member
+
+// id 와 name 을 붙여서 보여주기
+select id, name, id||name from member
+
+// id 와 name 사이에 : 보여주기
+select id, name, id||':'|| name from member
+
+// name 컬럼을 이름으로 보이게...별칭 (as 다음엔 '' 사용안함. )
+select id, name as 이름, age+1 as 한국나이 from member
+
+
+select id, name, id||name 비밀번호 from member
+
+// JDBC에서는 컬럼명, 별칭 중에서 별칭이 더 중요하다
+// 별칭이 주어지면 컬럼명은 의미가 없어짐.
+
+select distinct id from member		// distinct 중복제외.
+
+
+// 내림차순 : 값이 점점 줄어드는 정렬 desc
+// 오름차순 : 값이 점점 커지는 정렬 asc (디폴트는 오름차순 )
+
+// order by
+
+select * from member order by id asc
+select * from member order by id desc
+
+// member 테이블의 모든 레코드를 조회하는데 name을 기준으로 오름차순 정렬
+select * from member order by name asc
+
+
+// name으로 내림차순 정렬하고 만약 name이 같으면 age로 내림차순 정렬한다
+create table test10(
+id varchar2(6),
+name varchar2(21),
+age number(3)
+)
+
+insert into test10(id, name, age) values(1, 'a', 3)
+insert into test10(id, name, age) values(2, 'b', 2)
+insert into test10(id, name, age) values(3, 'c', 3)
+insert into test10(id, name, age) values(4, 'd', 2)
+insert into test10(id, name, age) values(5, 'e', 1)
+insert into test10(id, name, age) values(6, 'f', 4)
+insert into test10(id, name, age) values(7, 'g', 5)
+insert into test10(id, name, age) values(8, 'g', 4)
+insert into test10(id, name, age) values(9, 'a', 5)
+select * from test10
+select * from test10 order by name desc ,age desc
+
+
+
+
+
+
+
+
+
